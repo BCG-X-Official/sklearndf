d675faf07ada5261c2a563d2bd77d4da1e842588
Joerg Schneider <schneider.joerg@bcg.com>
2019-08-12 11:43:30 +0200

    move boruta pipeline test from test_transform to test_extra

--- test/test/gamma/sklearndf/transformation/test_extra.py
+++ test/test/gamma/sklearndf/transformation/test_extra.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
+from gamma.sklearndf.pipeline import PipelineDF
 from gamma.sklearndf.regression import RandomForestRegressorDF
+from gamma.sklearndf.transformation import SimpleImputerDF
 from gamma.sklearndf.transformation.extra import BorutaDF
 
 
@@ -17,3 +19,24 @@ def test_boruta_df() -> None:
 
     boruta_df = BorutaDF(estimator=RandomForestRegressorDF(), max_iter=10)
     boruta_df.fit(x, y)
+
+
+def test_boruta_pipeline(boston_df: pd.DataFrame, boston_target: str) -> None:
+    """ test a pipeline with on the boston dataset """
+
+    boruta_selector = PipelineDF(
+        steps=[
+            ("preprocess", SimpleImputerDF(strategy="median")),
+            (
+                "boruta",
+                BorutaDF(
+                    estimator=RandomForestRegressor(), n_estimators=10, max_iter=10
+                ),
+            ),
+        ]
+    )
+
+    x = boston_df.drop(columns=boston_target)
+    y = boston_df.loc[:, boston_target]
+
+    boruta_selector.fit(x, y)
