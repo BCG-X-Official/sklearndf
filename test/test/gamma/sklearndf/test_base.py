03f1df6be3fce4efddf09826cbc23341b7ae6c62
Jan Ittner <ittner.jan@bcg.com>
2019-09-18 08:33:56 +0200

    rename TestEstimator... classes to DummyEstimator...

--- test/test/gamma/sklearndf/test_base.py
+++ test/test/gamma/sklearndf/test_base.py
@@ -14,40 +14,40 @@ from gamma.sklearndf.pipeline import PipelineDF
 from gamma.sklearndf.transformation import OneHotEncoderDF
 
 
-class TestEstimator(BaseEstimator):
+class DummyEstimator(BaseEstimator):
     def __init__(self, l1=0, empty=None) -> None:
         self.l1 = l1
         self.empty = empty
 
 
-class TestEstimator2(BaseEstimator):
+class DummyEstimator2(BaseEstimator):
     def __init__(self, a=None, b=None) -> None:
         self.a = a
         self.b = b
 
 
-class TestEstimator3(BaseEstimator):
+class DummyEstimator3(BaseEstimator):
     def __init__(self, c=None, d=None) -> None:
         self.c = c
         self.d = d
 
 
-class TestEstimatorDF(BaseEstimatorWrapperDF[TestEstimator]):
+class DummyEstimatorDF(BaseEstimatorWrapperDF[DummyEstimator]):
     @classmethod
-    def _make_delegate_estimator(cls, *args, **kwargs) -> TestEstimator:
-        return TestEstimator(*args, **kwargs)
+    def _make_delegate_estimator(cls, *args, **kwargs) -> DummyEstimator:
+        return DummyEstimator(*args, **kwargs)
 
 
-class TestEstimator2DF(BaseEstimatorWrapperDF[TestEstimator2]):
+class DummyEstimator2DF(BaseEstimatorWrapperDF[DummyEstimator2]):
     @classmethod
-    def _make_delegate_estimator(cls, *args, **kwargs) -> TestEstimator2:
-        return TestEstimator2(*args, **kwargs)
+    def _make_delegate_estimator(cls, *args, **kwargs) -> DummyEstimator2:
+        return DummyEstimator2(*args, **kwargs)
 
 
-class TestEstimator3DF(BaseEstimatorWrapperDF[TestEstimator3]):
+class DummyEstimator3DF(BaseEstimatorWrapperDF[DummyEstimator3]):
     @classmethod
-    def _make_delegate_estimator(cls, *args, **kwargs) -> TestEstimator3:
-        return TestEstimator3(*args, **kwargs)
+    def _make_delegate_estimator(cls, *args, **kwargs) -> DummyEstimator3:
+        return DummyEstimator3(*args, **kwargs)
 
 
 def test_clone() -> None:
@@ -83,18 +83,18 @@ def test_clone_2() -> None:
 
 def test_clone_empty_array() -> None:
     # Regression test for cloning estimators with empty arrays
-    clf = TestEstimatorDF(empty=np.array([]))
+    clf = DummyEstimatorDF(empty=np.array([]))
     clf2 = clone(clf)
     assert_array_equal(clf.empty, clf2.empty)
 
-    clf = TestEstimatorDF(empty=sp.csr_matrix(np.array([[0]])))
+    clf = DummyEstimatorDF(empty=sp.csr_matrix(np.array([[0]])))
     clf2 = clone(clf)
     assert_array_equal(clf.empty.data, clf2.empty.data)
 
 
 def test_clone_nan() -> None:
     # Regression test for cloning estimators with default parameter as np.nan
-    clf = TestEstimatorDF(empty=np.nan)
+    clf = DummyEstimatorDF(empty=np.nan)
     clf2 = clone(clf)
 
     assert clf.empty is clf2.empty
@@ -107,7 +107,7 @@ def test_clone_sparse_matrices() -> None:
 
     for cls in sparse_matrix_classes:
         sparse_matrix = cls(np.eye(5))
-        clf = TestEstimatorDF(empty=sparse_matrix)
+        clf = DummyEstimatorDF(empty=sparse_matrix)
         clf_cloned = clone(clf)
         assert clf.empty.__class__ is clf_cloned.empty.__class__
         assert_array_equal(clf.empty.toarray(), clf_cloned.empty.toarray())
@@ -116,7 +116,7 @@ def test_clone_sparse_matrices() -> None:
 def test_clone_estimator_types() -> None:
     # Check that clone works for parameters that are types rather than
     # instances
-    clf = TestEstimatorDF(empty=TestEstimator)
+    clf = DummyEstimatorDF(empty=DummyEstimator)
     clf2 = clone(clf)
 
     assert clf.empty is clf2.empty
@@ -124,27 +124,27 @@ def test_clone_estimator_types() -> None:
 
 def test_repr() -> None:
     # Smoke test the repr of the base estimator.
-    my_estimator = TestEstimatorDF()
+    my_estimator = DummyEstimatorDF()
     repr(my_estimator)
 
-    test = TestEstimator2DF(TestEstimator3DF(), TestEstimator3DF())
+    test = DummyEstimator2DF(DummyEstimator3DF(), DummyEstimator3DF())
     assert repr(test) == (
-        "TestEstimator2DF(a=TestEstimator3DF(c=None, d=None),\n"
-        "                 b=TestEstimator3DF(c=None, d=None))"
+        "DummyEstimator2DF(a=DummyEstimator3DF(c=None, d=None),\n"
+        "                  b=DummyEstimator3DF(c=None, d=None))"
     )
 
-    some_est = TestEstimator2DF(a=["long_params"] * 1000)
-    assert len(repr(some_est)) == 640
+    some_est = DummyEstimator2DF(a=["long_params"] * 1000)
+    assert len(repr(some_est)) == 649
 
 
 def test_str() -> None:
     # Smoke test the str of the base estimator
-    my_estimator = TestEstimatorDF()
+    my_estimator = DummyEstimatorDF()
     str(my_estimator)
 
 
 def test_get_params() -> None:
-    test = TestEstimator2DF(TestEstimator3DF(), TestEstimator3DF())
+    test = DummyEstimator2DF(DummyEstimator3DF(), DummyEstimator3DF())
 
     assert "a__d" in test.get_params(deep=True)
     assert "a__d" not in test.get_params(deep=False)
