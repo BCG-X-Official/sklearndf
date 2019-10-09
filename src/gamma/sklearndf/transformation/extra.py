5cb6c64f30007ef433a76bc6a154cda8476c67a6
Joerg Schneider <schneider.joerg@bcg.com>
2019-10-09 19:20:42 +0200

    fix tests by adding missing abstract functions to outlierremovaldf

--- src/gamma/sklearndf/transformation/extra.py
+++ src/gamma/sklearndf/transformation/extra.py
@@ -23,7 +23,7 @@ import pandas as pd
 from boruta import BorutaPy
 from sklearn.base import BaseEstimator
 
-from gamma.sklearndf import TransformerDF
+from gamma.sklearndf import TransformerDF, _T
 from gamma.sklearndf.transformation import _ColumnSubsetTransformerWrapperDF
 from gamma.sklearndf.transformation._wrapper import _NDArrayTransformerWrapperDF
 from gamma.sklearndf.wrapper import df_estimator, MetaEstimatorWrapperDF
@@ -45,6 +45,12 @@ class OutlierRemoverDF(TransformerDF, BaseEstimator):
       samples in the above explanation (defaults to 3.0 as per Tukey's definition of
       far outliers)
     """
+
+    def get_params(self, deep=True) -> Dict[str, Any]:
+        return super().get_params(deep)
+
+    def set_params(self: _T, **kwargs) -> _T:
+        return super(**kwargs)
 
     def __init__(self, iqr_multiple: float = 3.0):
         super().__init__()
