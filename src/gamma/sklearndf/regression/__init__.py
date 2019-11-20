483c19b07a07f5767c21458220a7893d725ff9c5
Jan Ittner <ittner.jan@bcg.com>
2019-11-20 16:59:58 +0100

    include Base... classes as visible members of gamma modules and sphinx documentation

--- src/gamma/sklearndf/regression/__init__.py
+++ src/gamma/sklearndf/regression/__init__.py
@@ -3,5 +3,3 @@ Extended versions of all Scikit-Learn regressors with enhanced E2E support for d
 frames.
 """
 from ._regression import *
-
-__all__ = [member for member in _regression.__all__ if not member.startswith("Base")]
--- src/gamma/sklearndf/transformation/__init__.py
+++ src/gamma/sklearndf/transformation/__init__.py
@@ -3,7 +3,3 @@ Extended versions of all Scikit-Learn transformers with enhanced E2E support for
 frames.
 """
 from ._transformation import *
-
-__all__ = [
-    member for member in _transformation.__all__ if not member.startswith("Base")
-]
