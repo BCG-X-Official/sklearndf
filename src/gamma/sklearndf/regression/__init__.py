06e54dcddd18f2d36d9b24a8d455e015a1ccb572
Jörg Schneider <schneider.joerg@bcg.com>
2020-07-30 12:51:11 +0200

    move the sklearn version check up

--- src/gamma/sklearndf/regression/__init__.py
+++ src/gamma/sklearndf/regression/__init__.py
@@ -2,6 +2,13 @@
 Extended versions of all Scikit-Learn regressors with enhanced E2E support for data
 frames.
 """
+from sklearn import __version__ as __sklearn_version__
+from packaging.version import parse as __parse_version
+
 from ._regression import *
-from ._regression_v0_22 import *
-from ._regression_v0_23 import *
+
+if __parse_version(__sklearn_version__) >= __parse_version("0.22"):
+    from ._regression_v0_22 import *
+
+if __parse_version(__sklearn_version__) >= __parse_version("0.23"):
+    from ._regression_v0_23 import *
