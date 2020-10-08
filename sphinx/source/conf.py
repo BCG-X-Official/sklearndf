d0fba471df8d2201985c553b63bea87f6e6cc71a
Jan Ittner <ittner.jan@bcg.com>
2020-10-08 15:09:27 +0200

    update sklearndf sphinx build for new build process (#41)
    * update develop environment to ensure error-free documentation build
    * move conda_base and make to new directory sphinx/base
    * update sklearndf documentation for new build process
    * fix indentation of admonition in auto-generated rst docstring for scikit-learn estimators
    * align PyCharm folder exclusions with .gitignore
    * re-enable tutorial build
    * update nbsphinx to v0.7 for compatibility with nbconvert 6

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,7 +7,8 @@ Receives majority of configuration from pytools conf_base.py
 import os
 import sys
 
-sys.path.append(
+sys.path.insert(
+    0,
     os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
@@ -16,13 +17,18 @@ sys.path.append(
             os.pardir,
             "pytools",
             "sphinx",
-            "source",
+            "base",
         )
-    )
+    ),
 )
 
 from conf_base import set_config
 
 # ----- custom configuration -----
 
-set_config(globals(), project="sklearndf", modules=["pytools", "sklearndf"])
+set_config(
+    globals(),
+    project="sklearndf",
+    modules=["pytools", "sklearndf"],
+    html_logo="_static/gamma_sklearndf_logo.png",
+)
