0dc88340ff2d0f04df586cbcaad53ed34dade876
Jörg Schneider <schneider.joerg@bcg.com>
2020-10-29 11:12:43 +0100

    build: move make.py, adapt meta.yaml and add tox.ini

--- make.py
+++ make.py
@@ -7,10 +7,10 @@ import os
 import sys
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
-PYTOOLS_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, os.pardir, os.pardir, "pytools"))
+PYTOOLS_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, os.pardir, "pytools"))
 sys.path.insert(0, PYTOOLS_DIR)
 
 # noinspection PyUnresolvedReferences
-from condabuild import make
+from make import run_make
 
-make.run_make()
+run_make()
