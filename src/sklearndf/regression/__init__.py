d093f950dd2ebfd6c423eb2e325fbab520bf8a95
Jan Ittner <ittner.jan@bcg.com>
2020-11-13 18:36:07 +0100

    centralize parsing of scikit-learn versions

--- src/sklearndf/regression/__init__.py
+++ src/sklearndf/regression/__init__.py
@@ -2,15 +2,11 @@
 Extended versions of all Scikit-Learn regressors with enhanced E2E support for data
 frames.
 """
-from packaging.version import parse as __parse_version
-from sklearn import __version__ as __sklearn_version__
-
+from .. import __sklearn_0_22__, __sklearn_0_23__, __sklearn_version__
 from ._regression import *
 
-__sklearn_version = __parse_version(__sklearn_version)
-
-if __sklearn_version >= __parse_version("0.22"):
+if __sklearn_version__ >= __sklearn_0_22__:
     from ._regression_v0_22 import *
 
-if __sklearn_version >= __parse_version("0.23"):
+if __sklearn_version__ >= __sklearn_0_23__:
     from ._regression_v0_23 import *
