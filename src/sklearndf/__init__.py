aa690a4a66d9ca00976462e9949cb139facc1ce9
Jan Ittner <GitHub.com@ittner.de>
2020-11-14 01:35:42 +0100

    Bugfix/remove sgd regressor warning (#64)

--- src/sklearndf/__init__.py
+++ src/sklearndf/__init__.py
@@ -80,7 +80,14 @@ estimators, most notably :class:`.BorutaDF`,
 All `sklearndf` estimators are fully type hinted.
 """
 
+from packaging.version import parse as __parse_version
+from sklearn import __version__ as __sklearn_version__
+
 from ._sklearndf import *
 from ._version import __version__
 
 __version__ = __version__
+
+__sklearn_version__ = __parse_version(__sklearn_version__)
+__sklearn_0_22__ = __parse_version("0.22")
+__sklearn_0_23__ = __parse_version("0.23")
