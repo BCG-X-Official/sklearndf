0a628383254d02504b5611a992d54ecbbf5fec62
Jason Bentley <Bentley.Jason@bcg.com>
2021-01-11 12:15:19 +0000

    update docstring

--- src/sklearndf/__init__.py
+++ src/sklearndf/__init__.py
@@ -1,4 +1,6 @@
 """
+DataFrame support and feature traceability for scikit-learn.
+
 `sklearndf` augments more than 150 scikit-learn estimators for
 native support of data frames, while leaving the original API intact.
 """
