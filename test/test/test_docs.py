689520dd010ac5d542c874d64b614ed1944962d2
Jan Ittner <github.com@ittner.eu>
2021-02-25 12:54:50 +0100

    do not validate parameter docstrings/annotations for generated classes

--- test/test/test_docs.py
+++ test/test/test_docs.py
@@ -6,4 +6,20 @@ from pytools.api import DocValidator
 
 
 def test_docstrings() -> None:
-    assert DocValidator(root_dir="src").validate_docstrings(), "docstrings are valid"
+    assert DocValidator(
+        root_dir="src",
+        exclude_from_parameter_validation=(
+            "|".join(
+                f"(?:{pattern})"
+                for pattern in (
+                    (
+                        r"sklearndf\.(?:classification|regression|transformation)\."
+                        r"(?!extra\.).*"
+                    ),
+                    r"sklearndf\.(?:classification|regression)\.extra\.LGBM.*",
+                    r"sklearndf\.transformation\.extra\.Boruta.*",
+                    r"sklearndf\.pipeline\.(PipelineDF|FeatureUnionDF)\..*",
+                )
+            )
+        ),
+    ).validate_docstrings(), "docstrings are valid"
