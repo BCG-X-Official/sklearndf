c365c029cf108cdf70dd684f7019584ca5f0dfe6
Jan Ittner <ittner.jan@bcg.com>
2021-06-24 12:32:20 +0200

    FIX: restore compatibility with scikit-learn < 0.24 (#172)
    * FIX: load scikit-learn 0.24 transformers only when available
    
    * DOC: remove obsolete comments

--- src/sklearndf/transformation/_transformation_v0_22.py
+++ src/sklearndf/transformation/_transformation_v0_22.py
@@ -3,19 +3,6 @@ Core implementation of :mod:`sklearndf.transformation` loaded
 from sklearn 0.22 onwards
 """
 
-#
-# To create the DF class stubs:
-#
-# - generate a list of all child classes of TransformerMixin in PyCharm using the
-#   hierarchy view (^H)
-# - remove all abstract base classes and non-sklearn classes from the list
-# - unindent all lines
-# - use replace with regular expressions
-#   Find: (\w+)\([^\)]+\) \(([\w\.]+)\)
-#   Replace: @_df_transformer\nclass $1DF(TransformerDF, $1):\n    """\n    Wraps
-#            :class:`$2.$1`;\n    accepts and returns data frames.\n    """
-#            \n    pass\n\n
-# - clean up imports; import only the module names not the individual classes
 
 import logging
 
