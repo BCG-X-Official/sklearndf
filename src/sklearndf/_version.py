407a76b503f6377baafb0b190e64d394251fc052
Jan Ittner <github.com@ittner.eu>
2021-09-10 15:53:00 +0200

    BUILD: change version number to 2.0.dev0

--- src/sklearndf/_version.py
+++ src/sklearndf/_version.py
@@ -2,4 +2,4 @@
 Define the package version for gamma-sklearndf – done here so that it can be used
 without external dependencies (pandas, sklearn, ...).
 """
-__version__ = "2.0.0.dev0"
+__version__ = "2.0.dev0"
