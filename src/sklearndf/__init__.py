e11fe81eb0332753b5f6f47980239f407131e48f
Jan Ittner <ittner.jan@bcg.com>
2022-02-23 15:16:52 +0100

    BUILD: move __version__ to sklearndf.__init__ so flit can pick it up (#205)

--- src/sklearndf/__init__.py
+++ src/sklearndf/__init__.py
@@ -9,9 +9,8 @@ from packaging.version import parse as __parse_version
 from sklearn import __version__ as __sklearn_version__
 
 from ._sklearndf import *
-from ._version import __version__
 
-__version__: str  # noqa: F811
+__version__ = "1.1.4"
 
 __sklearn_version__ = __parse_version(__sklearn_version__)
 __sklearn_0_22__ = __parse_version("0.22")
