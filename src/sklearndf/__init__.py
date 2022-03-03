d2d3c2cb6a10241417885c8f82b457176149361f
Jan Ittner <github.com@ittner.eu>
2022-03-03 21:12:55 +0100

    BUILD: update version to 2.0.dev3

--- src/sklearndf/__init__.py
+++ src/sklearndf/__init__.py
@@ -10,7 +10,7 @@ from sklearn import __version__ as __sklearn_version__
 
 from ._sklearndf import *
 
-__version__ = "2.0.dev2"
+__version__ = "2.0.dev3"
 
 __sklearn_version__ = __parse_version(__sklearn_version__)
 __sklearn_0_22__ = __parse_version("0.22")
