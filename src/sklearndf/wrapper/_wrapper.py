a8104d6042c0035211b7560c70ba7e075b16951b
Jan Ittner <ittner.jan@bcg.com>
2022-07-19 15:41:26 +0200

    API: allow __main__ as a valid module to declare DF estimators in (#221)

--- src/sklearndf/wrapper/_wrapper.py
+++ src/sklearndf/wrapper/_wrapper.py
@@ -1420,7 +1420,8 @@ def _full_class_name(cls: type):
     except AttributeError as e:
         raise RuntimeError(f"cannot get module for {cls}") from e
 
-    module_name = public_module_prefix(module_name)
+    if module_name != "__main__":
+        module_name = public_module_prefix(module_name)
 
     return f"{module_name}.{cls.__qualname__}"
 
