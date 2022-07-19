955036459d17b247124560a09a62e0b6e227842a
Jan Ittner <ittner.jan@bcg.com>
2022-07-19 16:09:08 +0200

    DOC: enable local sphinx builds (#219)
    * DOC: enable local doc build using pytools bootstrap mechanism
    
    * DOC: update sphinx to 4.5
    
    * DOC: update the contribution guidelines
    
    * DOC: update environment.yml to use the latest nbsphinx version
    
    * DOC: fix indentation and punctuation in contribution_guide.rst
    
    * BUILD: update docs build in azure pipeline in line with latest pytools
    
    Co-authored-by: ben-howt <58532441+ben-howt@users.noreply.github.com>

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,28 +7,15 @@ Receives majority of configuration from pytools conf_base.py
 import os
 import sys
 
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            os.pardir,
-            os.pardir,
-            os.pardir,
-            "pytools",
-            "sphinx",
-            "base",
-        )
-    ),
-)
+_dir_base = os.path.join(os.path.dirname(os.path.dirname(__file__)), "base")
+sys.path.insert(0, _dir_base)
 
 from conf_base import set_config
 
-# ----- custom configuration -----
+# ----- set custom configuration -----
 
 set_config(
     globals(),
     project="sklearndf",
-    modules=["pytools", "sklearndf"],
-    html_logo="_static/gamma_sklearndf_logo.png",
+    html_logo=os.path.join("_static", "gamma_sklearndf_logo.png"),
 )
