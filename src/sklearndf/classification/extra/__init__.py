fc8f0158e4c1b5009b55153ac9f82ac1ba8b07b6
j-ittner <ittner.jan@bcg.com>
2022-08-18 20:26:33 +0200

    DOC: explain that 3rd party estimators must be installed explicitly

--- src/sklearndf/classification/extra/__init__.py
+++ src/sklearndf/classification/extra/__init__.py
@@ -1,4 +1,8 @@
 """
 Additional 3rd party classifiers that implement the `scikit-learn` interface.
+
+Note that 3rd party packages implementing the associated native estimators must be
+installed explicitly: they are not included in `sklearndf`'s package requirements to
+achieve a lean package footprint for default installs of `sklearndf`.
 """
 from ._extra import *
