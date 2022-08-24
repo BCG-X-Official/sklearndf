00bf94bdd4b775314bcd1db2603f37450ad912c3
j-ittner <ittner.jan@bcg.com>
2022-08-24 13:01:59 +0200

    REFACTOR: move sklearn version constants to a dedicated submodule

--- src/sklearndf/__init__.py
+++ src/sklearndf/__init__.py
@@ -5,17 +5,7 @@ Data frame support and feature traceability for `scikit-learn`.
 native support of data frames, while leaving the original API intact.
 """
 
-from packaging.version import Version as __Version
-from sklearn import __version__ as __sklearn_version_str
-
+from ._sklearn_version import *
 from ._sklearndf import *
 
 __version__ = "2.0.dev4"
-
-__sklearn_version__ = __Version(__sklearn_version_str)
-__sklearn_0_22__ = __Version("0.22")
-__sklearn_0_23__ = __Version("0.23")
-__sklearn_0_24__ = __Version("0.24")
-__sklearn_1_0__ = __Version("1.0")
-
-del __Version, __sklearn_version_str
