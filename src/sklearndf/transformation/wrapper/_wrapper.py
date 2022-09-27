5223e2e93aaebd63cad9136e9582ac0c90dd83a6
j-ittner <ittner.jan@bcg.com>
2022-09-27 12:44:43 +0200

    FIX: detect uses of deprecated OneHotEncoder param sparse

--- src/sklearndf/transformation/wrapper/_wrapper.py
+++ src/sklearndf/transformation/wrapper/_wrapper.py
@@ -34,6 +34,7 @@ from ... import (
     __sklearn_1_0__,
     __sklearn_1_1__,
     __sklearn_1_2__,
+    __sklearn_1_4__,
     __sklearn_version__,
 )
 from ...wrapper import TransformerWrapperDF
@@ -516,8 +517,12 @@ class OneHotEncoderWrapperDF(TransformerWrapperDF[OneHotEncoder], metaclass=ABCM
             sparse = self.native_estimator.sparse
             attr_sparse = "sparse"
         else:
-            sparse = self.native_estimator.sparse_output
             attr_sparse = "sparse_output"
+            sparse = self.native_estimator.sparse_output
+            if __sklearn_version__ < __sklearn_1_4__ and isinstance(
+                self.native_estimator.sparse, bool
+            ):
+                sparse = self.native_estimator.sparse
 
         if sparse:
             raise NotImplementedError(
