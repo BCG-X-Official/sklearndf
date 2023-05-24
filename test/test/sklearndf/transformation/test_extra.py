eaa49f4e698d2ff74123605e15d59097c2ec1e33
Jan Ittner <ittner.jan@bcg.com>
2023-05-24 07:16:17 +0200

    BUILD: use mamba for faster conda builds (#254)
    * BUILD: use mamba for faster conda builds
    
    * TEST: relax criteria for feature selection test
    
    * BUILD: run nightly builds on branch 2.2.x

--- test/test/sklearndf/transformation/test_extra.py
+++ test/test/sklearndf/transformation/test_extra.py
@@ -106,14 +106,18 @@ def test_feature_selection_pipeline_df(
 
     feature_selection_pipeline.fit(x, y)
 
+    selected_features = set(feature_selection_pipeline.feature_names_out_)
     try:
-        assert set(feature_selection_pipeline.feature_names_out_) == set(
-            feature_selector.selected_features_
-        )
+        assert selected_features == set(feature_selector.selected_features_)
     except AttributeError:
         pass
-    assert set(feature_selection_pipeline.feature_names_out_) in [
-        {"bmi", "bp", "s3", "s5"},
-        *({"bmi", "bp", "s3", "s5", extra} for extra in ["sex", "s1", "s2", "s6"]),
-        {"bmi", "bp", "s2", "s5", "s6"},
-    ]
+
+    assert {"bmi", "bp", "s5"}.issubset(
+        selected_features
+    ), "key features have been selected"
+
+    assert len(selected_features) <= 5, "no more than 5 features were selected"
+
+    assert (selected_features - {"bmi", "bp", "s5"}).issubset(
+        {"sex", "s1", "s2", "s3", "s6"}
+    ), "additional selected features were not completely irrelevant"
